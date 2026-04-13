d39e7d72fd60295a3115c6637326c9fea81f0158
Al3x370 <formicola04alex@gmail.com>
2026-04-13 18:25:00 +0200

    Deleted the Prop Angular code , and set the component to visualize in the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {ProselyteHome} from "./component/proselyte-home/proselyte-home";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ProselyteHome,
+        title: 'The Index - Home'
+    },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import {ProselyteHome} from "./component/proselyte-home/proselyte-home";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [ProselyteHome, RouterOutlet],
+  templateUrl: './app.html'
 })
-export class App {
-  protected readonly title = signal('IndexPrescript');
-}
+export class App {}
