59f00ff3706cd515a3e45ca0ecaa31cf8e471937
Alessandro.Formicola <alessandro.formicola@waveinformatica.com>
2026-04-15 15:07:28 +0200

    Added component that visualizes the prescript message

---
+++ src/app/component/prescript-message/prescript-message.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prescript-message',
+  imports: [],
+  templateUrl: './prescript-message.html',
+  styleUrl: './prescript-message.css',
+})
+export class PrescriptMessage {
+
+}
