66ce23054cdcf4ee835d2a757699ad074b9c77fe
Al3x370 <formicola04alex@gmail.com>
2026-05-01 19:47:59 +0200

    Implemented the component CSS, modified HTML to include a footer layout adjustments, in the TS added on start up logic for userName and clearPrescript methods.

--- src/app/component/prescript-message/prescript-message.ts
+++ src/app/component/prescript-message/prescript-message.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Footer} from "../shared/footer/footer";
 
 @Component({
   selector: 'app-prescript-message',
-  imports: [],
+  imports: [
+    Footer
+  ],
   templateUrl: './prescript-message.html',
   styleUrl: './prescript-message.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PrescriptMessage {
+  private readonly route = inject(ActivatedRoute);
 
+  protected userName = '';
+
+
+  ngOnInit() {
+    const state = history.state as { name?: string };
+    this.userName = state.name ?? 'Unknown';
+  }
+
+  protected NamePrefix() {
+    return "【To " + this.userName + "】";
+  }
+
+  protected clearPrescript() {
+
+  }
+
+  protected newPrescriptDisplay() {
+
+  }
 }
